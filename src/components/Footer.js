946f9971f21459e0f089318199cc39271a55c65e
Harsh-Veliyat <harshveliyat@gmail.com>
2021-05-10 15:13:24 +0530

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Footer.css";
+const Footer = () => {
+    return (
+        <div className="App">
+            <div class="footer">
+                <img src= "https://i.imgur.com/jKl82h1.jpg" alt="Hey" />
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
